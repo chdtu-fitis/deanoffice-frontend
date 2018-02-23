7267cc980639109398cc3de7b98cd8d80e595f20
Sauti <olha.parshakova@gmail.com>
2018-02-23 17:03:18 +0200

    add students page

--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {GroupComponent} from "../group/group.component";
-import {SpecialityComponent} from "../speciality/speciality.component";
-import {DiplomaSupplementComponent} from "../diploma-supplement/diploma-supplement.component";
-import {FormsModule} from "@angular/forms";
-import {AppRoutingModule} from "../../routes/routes";
-import {HttpClientModule} from "@angular/common/http";
-import {DegreeService} from "../../services/degree.service";
-import {GroupService} from "../../services/group.service";
-import {StudentService} from "../../services/student.service";
-import {DiplomaSupplementService} from "../../services/diploma-supplement.service";
-import {AppComponent} from "./app.component";
+import {GroupComponent} from '../group/group.component';
+import {SpecialityComponent} from '../speciality/speciality.component';
+import {DiplomaSupplementComponent} from '../diploma-supplement/diploma-supplement.component';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from '../../routes/routes';
+import {HttpClientModule} from '@angular/common/http';
+import {DegreeService} from '../../services/degree.service';
+import {GroupService} from '../../services/group.service';
+import {StudentService} from '../../services/student.service';
+import {DiplomaSupplementService} from '../../services/diploma-supplement.service';
+import {AppComponent} from './app.component';
 import { ExamReportComponent } from '../exam-report/exam-report.component';
+import { StudentsComponent } from '../students/students.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { ExamReportComponent } from '../exam-report/exam-report.component';
     GroupComponent,
     SpecialityComponent,
     DiplomaSupplementComponent,
-    ExamReportComponent
+    ExamReportComponent,
+    StudentsComponent,
   ],
   imports: [
     BrowserModule,
