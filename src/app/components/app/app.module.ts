02563bbe83c9d07e3325530b3b9891f9ae8f0e0b
TarasTurchenko <TurchTas.1551@gmail.com>
2018-02-28 18:22:05 +0200

    Added handleError`s function and some messages for errors

--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {GroupComponent} from "../group/group.component";
-import {SpecialityComponent} from "../speciality/speciality.component";
-import {DiplomaSupplementComponent} from "../diploma-supplement/diploma-supplement.component";
-import {FormsModule} from "@angular/forms";
-import {AppRoutingModule} from "../../routes/routes";
-import {HttpClientModule} from "@angular/common/http";
-import {DegreeService} from "../../services/degree.service";
-import {GroupService} from "../../services/group.service";
-import {StudentService} from "../../services/student.service";
-import {DiplomaSupplementService} from "../../services/diploma-supplement.service";
-import {AppComponent} from "./app.component";
-import { ExamReportComponent } from '../exam-report/exam-report.component';
+import {GroupComponent} from '../group/group.component';
+import {SpecialityComponent} from '../speciality/speciality.component';
+import {DiplomaSupplementComponent} from '../diploma-supplement/diploma-supplement.component';
+import {FormsModule} from '@angular/forms';
+import {AppRoutingModule} from '../../routes/routes';
+import {HttpClientModule} from '@angular/common/http';
+import {DegreeService} from '../../services/degree.service';
+import {GroupService} from '../../services/group.service';
+import {StudentService} from '../../services/student.service';
+import {DiplomaSupplementService} from '../../services/diploma-supplement.service';
+import {AppComponent} from './app.component';
+import {ExamReportComponent} from '../exam-report/exam-report.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +28,12 @@ import { ExamReportComponent } from '../exam-report/exam-report.component';
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [DegreeService, GroupService, StudentService, DiplomaSupplementService],
+  providers: [
+    DegreeService,
+    GroupService,
+    StudentService,
+    DiplomaSupplementService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/degree.service.ts
+++ src/app/services/degree.service.ts
@@ -1,13 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {HttpClient} from '@angular/common/http';
 import {Degree} from '../models/Degree';
 import {Observable} from 'rxjs/Observable';
-import {of} from 'rxjs/observable/of';
-import {catchError, map, tap} from 'rxjs/operators';
-
-const httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json'})
-};
+import {catchError} from 'rxjs/operators';
+import {handleError} from './httpErrors';
 
 @Injectable()
 export class DegreeService {
@@ -19,28 +15,12 @@ export class DegreeService {
     // getDegrees(): Degree[] {
     //   return [{id: 1, name: "бакалавр"},{id: 3, name: "магістр"}];
     // }
+    // TODO: Прохання такі речі за собою прибирати
 
     getDegrees(): Observable<Degree[]> {
         return this.http.get<Degree[]>(this.degreesUrl)
             .pipe(
-                catchError(this.handleError('getDegrees', []))
+                catchError(handleError('getDegrees', []))
             );
-    }
-
-    /**
-     * Handle Http operation that failed.
-     * Let the app continue.
-     * @param operation - name of the operation that failed
-     * @param result - optional value to return as the observable result
-     */
-    private handleError<T>(operation = 'operation', result?: T) {
-        return (error: any): Observable<T> => {
-            // TODO: send the error to remote logging infrastructure
-            console.error(error); // log to console instead
-            // TODO: better job of transforming error for user consumption
-            // this.log(`${operation} failed : ${error.message}`);
-            // Let the app keep running by returning an empty result.
-            return of(result as T);
-        }
     }
 }
