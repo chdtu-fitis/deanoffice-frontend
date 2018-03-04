18b15e90f27c475cf25459c39dd2b9163b611427
OlegSamardak <os1998os@ukr.net>
2018-03-04 10:52:20 +0200

    added courses-for-groups to routing

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -3,16 +3,20 @@ import {RouterModule, Routes} from '@angular/router';
 import {DiplomaSupplementComponent} from '../components/diploma-supplement/diploma-supplement.component';
 import {AppComponent} from '../components/app/app.component';
 import {ExamReportComponent} from "../components/exam-report/exam-report.component";
+import {CoursesForGroupsComponent} from "../components/courses-for-groups/courses-for-groups.component";
+import {CoursesForGroupsModule} from "../components/courses-for-groups/courses-for-groups.module";
 
 const routes: Routes = [
     {path: 'diplsuppl', component: DiplomaSupplementComponent},
     {path: 'examreport', component: ExamReportComponent},
-    {path: '', component: DiplomaSupplementComponent, pathMatch: 'full'}
+    {path: '', component: DiplomaSupplementComponent, pathMatch: 'full'},
+    {path: 'courses-for-groups', component: CoursesForGroupsComponent}
 ];
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes),
+      CoursesForGroupsModule
     ],
     exports: [
         RouterModule
