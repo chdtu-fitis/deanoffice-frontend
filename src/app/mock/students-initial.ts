55696c8b13ebe5e974c0f9977e99dbb030f3699b
Sauti <olha.parshakova@gmail.com>
2018-03-05 19:00:53 +0200

    fix mock data

--- src/app/mock/students-initial.ts
+++ src/app/mock/students-initial.ts
@@ -17,5 +17,4 @@ const studentMock = {
 
 export default new Array(20)
   .fill(studentMock)
-  .map((elem, id) => ({ ...elem, id, 'student.surname': elem.surname + id }))
-
+  .map((elem, id) => ({ ...elem, id }));
