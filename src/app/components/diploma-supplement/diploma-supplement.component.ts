e38a506cf137b7df73b432f034ce65040867237f
gzaspa <gzaspa@ukr.net>
2018-03-05 23:08:39 +0200

    correct merge related problems with diploma supplement

--- src/app/components/diploma-supplement/diploma-supplement.component.ts
+++ src/app/components/diploma-supplement/diploma-supplement.component.ts
@@ -5,6 +5,7 @@ import {GroupService} from '../../services/group.service';
 import {StudentGroup} from '../../models/StudentGroup';
 import {StudentService} from '../../services/student.service';
 import {Student} from '../../models/Student';
+import {DiplomaSupplementService} from "../../services/diploma-supplement.service";
 
 @Component({
   selector: 'diploma-supplement',
@@ -15,8 +16,11 @@ export class DiplomaSupplementComponent implements OnInit {
   degrees: Degree[];
   groups: StudentGroup[];
   students: Student[];
+  studentsSelected: boolean;
+  message: string;
 
-  constructor(private degreeService: DegreeService, private groupService: GroupService, private studentService: StudentService) {
+  constructor(private degreeService: DegreeService, private groupService: GroupService,
+              private studentService: StudentService, private diplomaSupplementService: DiplomaSupplementService) {
   }
 
   ngOnInit() {
@@ -33,6 +37,24 @@ export class DiplomaSupplementComponent implements OnInit {
 
   onGroupChange(groupId: string): void {
     this.groupService.getGroupStudents(groupId)
-      .subscribe(students => this.students = students);
+      .subscribe(students => {this.students = students;
+        for (var student of this.students) {student.selected = true;}
+        this.studentsSelected = true;
+      });
+  }
+
+  onSelectAllStudents(checked: boolean): void {
+    for (var student of this.students) {
+      student.selected = checked;
+    }
+  }
+
+  onFormSupplement(): void {
+    this.message = "";
+    for (var student of this.students) {
+      if (student.selected){
+        this.diplomaSupplementService.buildDiplomaSupplement(""+student.id);
+      }
+    }
   }
 }
