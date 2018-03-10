fe53e7528a3974bfd2592c932643edd9ba5d20be
Sauti <olha.parshakova@gmail.com>
2018-03-10 19:48:15 +0200

    add filter by payment

--- src/app/components/students/students-filters/students-filters.component.ts
+++ src/app/components/students/students-filters/students-filters.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 import {StudentGroup} from '../../../models/StudentGroup';
 import {StudentDegree} from '../../../models/StudentDegree';
@@ -11,19 +11,27 @@ import {months} from '../constants.js';
     styleUrls: ['./students-filters.component.scss'],
 })
 export class StudentsFiltersComponent {
-  filters: FormGroup;
+  model = {
+    group: '',
+    payment: [
+      {label: 'Бюджет', value: 'BUDGET', selected: true},
+      {label: 'Контракт', value: 'CONTRACT', selected: true},
+    ],
+    birthDate: '',
+    birthMonth: ''
+  };
+  form: FormGroup;
   months: string[] = months;
   @Input() groups: StudentGroup[];
   @Input() students: StudentDegree[];
   @Output() applyFilters = new EventEmitter();
 
   constructor(private fb: FormBuilder) {
-    this.filters = fb.group({
-      group: '',
-      payment: this.fb.array([]),
-      birthDate: '',
-      birthMonth: ''
-    });
+    this.buildForm();
+  }
+
+  get payment(): FormArray {
+    return this.form.get('payment') as FormArray;
   }
 
   setFilters() {
@@ -32,12 +40,23 @@ export class StudentsFiltersComponent {
   }
 
   resetFilters() {
-    this.filters.reset();
+    this.buildForm();
     this.applyFilters.emit(this.students);
   }
 
+  private buildForm() {
+    this.form = this.fb.group({
+      group: this.model.group,
+      payment: this.fb.array(this.model.payment.map(entry =>
+        this.fb.group(entry)),
+      ),
+      birthDate: this.model.birthDate,
+      birthMonth: this.model.birthMonth,
+    });
+  }
+
   private filter(entry: StudentDegree): Boolean {
-    const { group, payment, birthDate, birthMonth } = this.filters.value;
+    const { group, payment, birthDate, birthMonth } = this.form.value;
     const isGroupMatch = group ? entry.studentGroup.name === group : true;
 
     let isBirthDateMatch = true;
@@ -54,8 +73,11 @@ export class StudentsFiltersComponent {
         : false;
     }
 
-    // todo filter by payment
+    const payments = payment
+      .filter(val => val.selected)
+      .map(val => val.value);
+    const isPaymentMatch = payments.includes(entry.payment);
 
-    return isGroupMatch && isBirthDateMatch && isBirthMonthMatch;
+    return isGroupMatch && isBirthDateMatch && isBirthMonthMatch && isPaymentMatch;
   }
 }
