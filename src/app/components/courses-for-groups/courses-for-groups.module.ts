183aa5ce59e54e666b2e5989bcc4eb59d419011a
OlegSamardak <os1998os@ukr.net>
2018-03-10 16:03:40 +0200

    reworked bootstrap grid accordin to mockup. Added table and panel, placeholder for select doesnt work

--- src/app/components/courses-for-groups/courses-for-groups.module.ts
+++ src/app/components/courses-for-groups/courses-for-groups.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesForGroupsComponent } from './courses-for-groups.component';
 import {HttpClientModule} from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { StudiedCoursesComponent } from './studied-courses/studied-courses.component';
 import { AddedCoursesComponent } from './added-courses/added-courses.component';
 import { CourseCreationComponent } from './course-creation/course-creation.component';
@@ -11,7 +11,8 @@ import { CourseCreationComponent } from './course-creation/course-creation.compo
   imports: [
     CommonModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   declarations: [
     CoursesForGroupsComponent,
