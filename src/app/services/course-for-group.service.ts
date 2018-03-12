174023ca426fcb11827004a6c2233493a04c335f
OlegSamardak <os1998os@ukr.net>
2018-03-12 23:45:49 +0200

    changed courses by specialization method to restfull.

--- src/app/services/course-for-group.service.ts
+++ src/app/services/course-for-group.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {CourseForGroup} from '../models/CourseForGroup';
+import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class CourseForGroupService {
@@ -8,11 +9,7 @@ export class CourseForGroupService {
   constructor(private http: HttpClient) {
   }
 
-  getCoursesBySpecialization(specializationId) {
-    return this.http.get<CourseForGroup>(`/coursesforgroups/${specializationId}/courses`).toPromise();
-  }
-
-  setCoursesForGroup(body, groupId) {
-    return this.http.post(`/coursesforgroups/${groupId}/courses`, body).toPromise();
+  getCoursesBySpecialization(specializationId): Observable<CourseForGroup[]> {
+    return this.http.get<CourseForGroup[]>(`/specialization/${specializationId}/courses`);
   }
 }
