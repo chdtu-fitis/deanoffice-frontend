49a9377df4ff38614263f6792c081c807f515f12
Sauti <olha.parshakova@gmail.com>
2018-03-17 21:23:13 +0200

    add personal info form

--- src/app/components/students/student-personal-info/student-personal-info.component.ts
+++ src/app/components/students/student-personal-info/student-personal-info.component.ts
@@ -1,5 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
 import {ModalDirective} from 'ngx-bootstrap';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 import {IAppModal} from '../../shared/modal.interface';
 
@@ -9,5 +10,38 @@ import {IAppModal} from '../../shared/modal.interface';
     styleUrls: ['./student-personal-info.component.scss'],
 })
 export class StudentPersonalInfoComponent implements IAppModal {
+  form: FormGroup;
   @ViewChild('modal') modal: ModalDirective;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      patronimic: ['', Validators.required],
+      nameEng: '',
+      surnameEng: '',
+      patronimicEng: '',
+      telephone: '',
+      sex: ['', Validators.required],
+      birthDate: ['', Validators.required],
+      registrationAddress: '',
+      actualAddress: '',
+      studentCardNumber: '',
+      school: '',
+      fatherName: '',
+      fatherPhone: '',
+      fatherInfo: '',
+      motherName: '',
+      motherPhone: '',
+      motherInfo: '',
+      motherNotes: '',
+      motherEmail: '',
+      notes: '',
+      email: '',
+    })
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
 }
