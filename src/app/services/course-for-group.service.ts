022a7a78effb4b130cadfed293b52284f3155efb
TarasTurchenko <TurchTas.1551@gmail.com>
2018-03-26 12:44:51 +0300

    Refactor: update url in services

--- src/app/services/course-for-group.service.ts
+++ src/app/services/course-for-group.service.ts
@@ -1,18 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {CourseForGroup} from '../models/CourseForGroup';
 
 @Injectable()
 export class CourseForGroupService {
-
-  constructor(private http: HttpClient) {
-  }
-
-  getCoursesBySpecialization(specializationId) {
-    return this.http.get<CourseForGroup>(`/coursesforgroups/${specializationId}/courses`).toPromise();
-  }
-
-  setCoursesForGroup(body, groupId) {
-    return this.http.post(`/coursesforgroups/${groupId}/courses`, body).toPromise();
-  }
 }
--- src/app/services/diploma-supplement.service.ts
+++ src/app/services/diploma-supplement.service.ts
@@ -1,40 +1,17 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import {catchError} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
-import {of} from 'rxjs/observable/of';
 import {environment} from '../../environments/environment';
 
 @Injectable()
 export class DiplomaSupplementService {
   private documentsUrl = `${environment.apiUrl}/documents`;
 
-  constructor(private http: HttpClient) {
-  }
-
   buildDiplomaSupplement(studentId: string): void {
     const url = `${this.documentsUrl}/diplomas/supplements/studentdegrees/${studentId}`;
-    window.open(url, "_blank");
+    window.open(url, '_blank');
   }
 
   buildGradePercent(groupId: string): void {
     const url = `${this.documentsUrl}/percentagereport/groups/${groupId}`;
-    window.open(url, "_blank");
-  }
-  /**
-   * Handle Http operation that failed.
-   * Let the app continue.
-   * @param operation - name of the operation that failed
-   * @param result - optional value to return as the observable result
-   */
-  private handleError<T>(operation = 'operation', result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-      // TODO: better job of transforming error for user consumption
-      // this.log(`${operation} failed : ${error.message}`);
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    }
+    window.open(url, '_blank');
   }
 }
