99bf2424ec88331d9296c7abf886377469815342
Sauti <olha.parshakova@gmail.com>
2018-03-31 22:19:48 +0300

    add file size check

--- src/app/components/students/photo-upload/photo-upload.component.ts
+++ src/app/components/students/photo-upload/photo-upload.component.ts
@@ -1,5 +1,7 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
+import { maxFileSize } from '../constants.js';
+
 @Component({
     selector: 'app-photo-upload',
     templateUrl: './photo-upload.component.html',
@@ -7,6 +9,7 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 })
 export class PhotoUploadComponent {
   preview: WindowBase64;
+  error: string;
   @Input() set photo(data) {
     if (data) {
       this.generatePreviewImage(data);
@@ -17,8 +20,13 @@ export class PhotoUploadComponent {
   @Output() onLoad = new EventEmitter();
 
   onSelectFile(event) {
-    const files = event.target.files;
+    this.error = '';
+    const files: File[] = event.target.files;
     if (files && files[0]) {
+      if (files[0].size > maxFileSize) {
+        this.error = 'Файл занадто великий.';
+        return;
+      }
       this.onLoad.emit(files[0]);
       this.generatePreviewImage(files[0]);
     }
