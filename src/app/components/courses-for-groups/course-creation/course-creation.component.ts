203cdd2dd05dec0139f91de378e15cb1fa0ec425
unknown <os1998os@ukr.net>
2018-04-01 19:02:18 +0300

    added method for course creation and linked to button

--- src/app/components/courses-for-groups/course-creation/course-creation.component.ts
+++ src/app/components/courses-for-groups/course-creation/course-creation.component.ts
@@ -1,17 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import {Course} from '../../../models/Course';
+import {KnowledgeControl} from '../../../models/KnowlegeControl';
+import {CourseName} from '../../../models/CourseName';
+import {CourseService} from '../../../services/course.service';
 
 @Component({
   selector: 'course-creation',
   templateUrl: './course-creation.component.html',
-  styleUrls: ['./course-creation.component.scss']
+  styleUrls: ['./course-creation.component.scss'],
+  providers: [CourseService]
 })
 export class CourseCreationComponent implements OnInit {
-  course: Course;
+  course: Course = new Course();
+  knowledgeControl: KnowledgeControl;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private courseService: CourseService) {
   }
 
+  ngOnInit() {
+
+  }
+
+  createCourse(){
+    this.courseService.createCourse(this.course).subscribe(() => {
+
+    })
+  }
 }
