c32d5dda7bb64c75a8751b806bcbc290f47fe260
unknown <os1998os@ukr.net>
2018-04-03 14:08:40 +0300

    added spinner optioning.

--- src/app/components/loading/loading.component.ts
+++ src/app/components/loading/loading.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'loading',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./loading.component.scss']
 })
 export class LoadingComponent implements OnInit {
+  @Input()size: number;
 
   constructor() { }
 
