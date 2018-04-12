860e3c46625d3ecb0de08412514be4dcf7058b1b
Grygoriy Zaspa <gzaspa@ukr.net>
2018-04-12 17:37:48 +0300

    Update app.module.ts

--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -3,23 +3,23 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 
+import {AppRoutingModule} from '../../routes/routes';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './header/header.component';
 import {GroupComponent} from '../group/group.component';
 import {SpecialityComponent} from '../speciality/speciality.component';
 import {DiplomaSupplementComponent} from '../diploma-supplement/diploma-supplement.component';
-import {AppRoutingModule} from '../../routes/routes';
+import {ExamReportComponent} from '../exam-report/exam-report.component';
 import {DegreeService} from '../../services/degree.service';
 import {GroupService} from '../../services/group.service';
 import {StudentService} from '../../services/student.service';
 import {DiplomaSupplementService} from '../../services/diploma-supplement.service';
-import {AppComponent} from './app.component';
-import {ExamReportComponent} from '../exam-report/exam-report.component';
-import {HeaderComponent} from './header/header.component';
-import {GridModule} from '../grid/grid.module';
-import {StudentsModule} from '../students/students.module';
-import {SharedModule} from '../shared/shared.module';
 import {CourseForGroupService} from '../../services/course-for-group.service';
 import {GeneralService} from '../../services/general.service'
 import {ExamReportService} from "../../services/exam-report.service";
+import {GridModule} from '../grid/grid.module';
+import {StudentsModule} from '../students/students.module';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [
