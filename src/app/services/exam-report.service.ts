458a84aaa6bffd1cc64827f5304d59d3e58a04ed
gzaspa <gzaspa@ukr.net>
2018-04-24 22:32:54 +0300

    fix: make exam report without opening new tab

--- src/app/services/exam-report.service.ts
+++ src/app/services/exam-report.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import {environment} from '../../environments/environment';
+import {FileService} from "./file-service";
 
 @Injectable()
 export class ExamReportService {
   private documentsUrl = `${environment.apiUrl}/documents`;
 
-  constructor() { }
+  constructor(private fileService: FileService) { }
 
   buildExamReport(groupId: number, courseId: number): void {
       const url = `${this.documentsUrl}/exam-report/groups/${groupId}/courses/${courseId}/docx`;
-      window.open(url, '_blank');
+      this.fileService.downloadFile(url);
   }
 
   buildExamReportJournalDoc(groupId: number, semester: number): void {
     const url = `${this.documentsUrl}/coursereport/groups/${groupId}/${semester}`;
-    window.open(url, '_blank');
+    this.fileService.downloadFile(url);
   }
 }
