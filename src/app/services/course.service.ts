87114c5951e4f80e024d4fc9dc79ff341ca1522d
Andrei Zaiats <andrei.zaiats@gmail.com>
2018-04-25 20:57:01 +0300

    Fix links for API requests.

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Course} from "../models/Course";
 import {Observable} from "rxjs/Observable";
 import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
 
 @Injectable()
 export class CourseService {
@@ -10,15 +11,15 @@ export class CourseService {
   }
 
   getCoursesBySemester(semester): Observable<Course[]> {
-    return this.http.get<Course[]>(`http://localhost:8080/courses?semester=${semester}`);
+    return this.http.get<Course[]>(`${environment.apiUrl}/courses?semester=${semester}`);
   }
 
   createCourse(course: Course){
-    return this.http.post(`http://localhost:8080/courses`, course);
+    return this.http.post(`${environment.apiUrl}/courses`, course);
   }
 
   getCourseNames() {
-    return this.http.get(`http://localhost:8080/courses/names`);
+    return this.http.get(`${environment.apiUrl}/courses/names`);
   }
 
 }
