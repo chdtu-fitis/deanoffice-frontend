6de8fa05f6e316637f9aac0dc1e1b6c5c0815460
TarasTurchenko <TurchTas.1551@gmail.com>
2018-04-27 22:05:50 +0300

    Disabled autocomplete in student-typeahead

--- src/app/components/speciality/speciality.component.ts
+++ src/app/components/speciality/speciality.component.ts
@@ -9,46 +9,43 @@ import {Speciality} from '../../models/Speciality';
 })
 export class SpecialityComponent implements OnInit {
 
-    buttonText = 'Показати неактуальні';
-    isActual = true;
-    selected = [];
-    specialities: Speciality[] = [];
-    rows: Speciality[] = [];
-    columns = [
-        {name: 'Шифр', prop: 'code'},
-        {name: 'Назва', prop: 'name'},
-        {name: 'Назва Англійською', prop: 'nameEng'},
-        {name: 'Галузь Знань', prop: 'fieldOfStudy'},
-        {name: 'Галузь Знань Англійською', prop: 'fieldOfStudyEng'}
-    ];
+  buttonText = 'Показати неактуальні';
+  isActual = true;
+  selected: Speciality[] = [];
+  specialities: Speciality[] = [];
+  rows: Speciality[] = [];
+  columns = [
+    {name: 'Шифр', prop: 'code'},
+    {name: 'Назва', prop: 'name'},
+    {name: 'Назва Англійською', prop: 'nameEng'},
+    {name: 'Галузь Знань', prop: 'fieldOfStudy'},
+    {name: 'Галузь Знань Англійською', prop: 'fieldOfStudyEng'}
+  ];
 
-    constructor(private specialityService: SpecialityService) {
-    }
+  constructor(private specialityService: SpecialityService) {}
 
-    ngOnInit() {
-        this.getSpecialities();
-    }
+  ngOnInit() {
+    this.getSpecialities();
+  }
 
-    onSelect({selected}) {
-        console.log('Select Event', selected, this.selected);
+  getSpecialities() {
+    this.specialityService.getSpecialities()
+      .subscribe((speciality: Speciality[]) => {
+        this.specialities = speciality;
+        this.rows = speciality.filter(value => value.active === this.isActual);
+      });
+  }
 
-        this.selected.splice(0, this.selected.length);
-        this.selected.push(...selected);
-    }
+  onSelect({selected}) {
+    console.log('Select Event', selected, this.selected);
 
-    actual() {
-        this.isActual = !this.isActual;
-        this.buttonText = this.isActual ? 'Показати неактуальні' : 'Показати актуальні';
-        this.rows = this.specialities.filter(value => value.active === this.isActual);
-    }
+    this.selected.splice(0, this.selected.length);
+    this.selected.push(...selected);
+  }
 
-    getSpecialities() {
-        const stream = this.specialityService.getSpecialities();
-
-        stream.subscribe((speciality: Speciality[]) => {
-            this.specialities = speciality;
-            this.rows = speciality.filter(value => value.active === this.isActual);
-        });
-    }
-
+  actual() {
+    this.isActual = !this.isActual;
+    this.buttonText = this.isActual ? 'Показати неактуальні' : 'Показати актуальні';
+    this.rows = this.specialities.filter(value => value.active === this.isActual);
+  }
 }
