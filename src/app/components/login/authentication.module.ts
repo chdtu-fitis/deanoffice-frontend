5e9cf9f89a4d97178c6283f38ba7f602a3108f25
TarasTurchenko <TurchTas.1551@gmail.com>
2018-05-03 12:23:33 +0300

    Fixed routes

--- src/app/components/login/authentication.module.ts
+++ src/app/components/login/authentication.module.ts
@@ -3,7 +3,7 @@ import {LoginComponent} from './login.component';
 import {FormsModule} from '@angular/forms';
 import {AppRoutingModule} from '../../routes/routes';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {AuthGuard} from '../../services/auth/auth.guard';
+import {DashboardGuard, LoginGuard} from '../../services/auth/auth.guard';
 import {AuthenticationService} from '../../services/auth/authentication.service';
 import {TokenInterceptor} from '../../services/auth/token.interceptor';
 import {BrowserModule} from '@angular/platform-browser';
@@ -19,7 +19,8 @@ import {BrowserModule} from '@angular/platform-browser';
     HttpClientModule,
   ],
   providers: [
-    AuthGuard,
+    DashboardGuard,
+    LoginGuard,
     AuthenticationService,
     {
       provide: HTTP_INTERCEPTORS,
