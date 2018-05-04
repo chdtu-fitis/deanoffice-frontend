0de8ad48c0b04a5af7085f1e963fc3e12b41c2a5
TarasTurchenko <TurchTas.1551@gmail.com>
2018-05-04 17:54:02 +0300

    Use custom table for groups

--- src/app/components/group/group-table/group-table.component.ts
+++ src/app/components/group/group-table/group-table.component.ts
@@ -1,7 +1,5 @@
-import {Component, EventEmitter, Input, Output, TemplateRef, ViewChild} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {StudentGroup} from '../../../models/StudentGroup';
-
-import { translations } from '../translations.js';
 
 @Component({
   selector: 'app-group-table',
@@ -10,44 +8,4 @@ import { translations } from '../translations.js';
 })
 export class GroupTableComponent {
   @Input('rows') rows: StudentGroup[];
-  @ViewChild('tuitionFormTemplate') tuitionFormTemplate: TemplateRef<any>;
-  @ViewChild('tuitionTermTemplate') tuitionTermTemplate: TemplateRef<any>;
-
-  getColumns(): Object[] {
-    const columns = [
-      'name',
-      'specialization.degree.name',
-      'specialization.name',
-      'creationYear',
-      'tuitionForm',
-      'tuitionTerm',
-      'studySemesters',
-      'studyYears',
-      'beginYears'
-    ];
-
-    const templatesMap = {
-      'tuitionForm': {cellTemplate: this.tuitionFormTemplate},
-      'tuitionTerm': {cellTemplate: this.tuitionTermTemplate},
-      'selected': {
-        name: '',
-        sortable: false,
-        canAutoResize: false,
-        draggable: false,
-        resizable: false,
-        headerCheckboxable: true,
-        checkboxable: true,
-        width: 30
-      }
-    };
-
-    return ['selected', ...columns].map(prop => {
-      const column = templatesMap[prop];
-      return { prop, name: translations[prop], ...column };
-    });
-  }
-
-  getRowIdentity(row) {
-    return row.id;
-  }
 }
