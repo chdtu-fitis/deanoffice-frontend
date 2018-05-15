d2b04ad887536eed759f394911ba4a1c9c855e9d
unknown <os1998os@ukr.net>
2018-05-15 10:31:35 +0300

    setted  col width

--- src/app/components/group/group-table/group-table.component.ts
+++ src/app/components/group/group-table/group-table.component.ts
@@ -11,17 +11,6 @@ import {TuitionForm} from '../../../models/tuition-form.enum';
 export class GroupTableComponent {
   @Input('rows') rows: StudentGroup[];
   @Input() searchText: string;
-  cols = [
-    {name: 'Назва', prop: 'name'},
-    {name: 'Спеціалізація', prop: 'specialization.name'},
-    {name: 'Ступінь', prop: 'specialization.degree.name'},
-    {name: 'Форма навчання', prop: 'tuitionForm'},
-    {name: 'Термін навчання', prop: 'tuitionTerm'},
-    {name: 'Рік створення', prop: 'creationYear'},
-    {name: 'Рік початку', prop: 'beginYears'},
-    {name: 'Років начання', prop: 'studyYears'},
-    {name: 'Семестрів', prop: 'studySemesters'},
-  ];
 
   @Input() loading: boolean;
 
