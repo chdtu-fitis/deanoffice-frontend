b26af39a787f1d2847cc5b9663e4e45eb8769add
TarasTurchenko <TurchTas.1551@gmail.com>
2018-06-27 19:32:31 +0300

    Added index.ts for services and guards

--- src/app/components/app/app.module.ts
+++ src/app/components/app/app.module.ts
@@ -12,23 +12,8 @@ import {PipeModule} from '../../pipes/pipe.module';
 import {AuthenticationModule} from '../login/authentication.module';
 import {SharedModule} from '../shared/shared.module';
 
-import {DashboardGuard, LoginGuard} from '../../services/auth/auth.guard';
-
-import {SpecializationService} from '../../services/specialization.service';
-import {DepartmentService} from '../../services/department.service';
-import {GradeService} from '../../services/grade.service';
-import {SpecialityService} from '../../services/speciality.service';
-import {FileService} from '../../services/file-service';
-import {AuthenticationService} from '../../services/auth/authentication.service';
-import {DegreeService} from '../../services/degree.service';
-import {GroupService} from '../../services/group.service';
-import {StudentService} from '../../services/student.service';
-import {DiplomaSupplementService} from '../../services/diploma-supplement.service';
-import {CourseForGroupService} from '../../services/course-for-group.service';
-import {GeneralService} from '../../services/general.service'
-import {ExamReportService} from '../../services/exam-report.service';
-
 import {appRoutes} from './app-routes';
+import {GUARDS, SERVICES} from '../../services';
 
 @NgModule({
   declarations: [
@@ -45,22 +30,8 @@ import {appRoutes} from './app-routes';
     PipeModule
   ],
   providers: [
-    DegreeService,
-    GroupService,
-    StudentService,
-    DiplomaSupplementService,
-    GeneralService,
-    CourseForGroupService,
-    ExamReportService,
-    SpecialityService,
-    FileService,
-    GradeService,
-    SpecialityService,
-    SpecializationService,
-    DepartmentService,
-    AuthenticationService,
-    DashboardGuard,
-    LoginGuard,
+    ...SERVICES,
+    ...GUARDS,
     AuthenticationModule.tokenInterceptor()
   ],
   bootstrap: [AppComponent]
