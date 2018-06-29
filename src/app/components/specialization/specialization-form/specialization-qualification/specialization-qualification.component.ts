9f9d5a3f426e0f99cd7dc08e762228feb4c722da
TarasTurchenko <TurchTas.1551@gmail.com>
2018-06-29 08:21:01 +0300

    Added placeholder for qualification

--- src/app/components/specialization/specialization-form/specialization-qualification/specialization-qualification.component.ts
+++ src/app/components/specialization/specialization-form/specialization-qualification/specialization-qualification.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {QualificationService} from '../services/qualification.service';
-import {ProfessionalQualification} from "../models/professional-qualification";
+import {ProfessionalQualification} from '../models/professional-qualification';
 
 @Component({
   selector: 'specialization-qualification',
@@ -16,5 +16,12 @@ export class SpecializationQualificationComponent {
   loadData(specializationId: number = 0): void {
     this._service.getLast(specializationId)
       .subscribe((qualification: ProfessionalQualification) => this.qualification = qualification);
+  }
+
+  hasData(): boolean {
+    if (!this.qualification) {
+      return false;
+    }
+    return Object.keys(this.qualification).length > 0;
   }
 }
