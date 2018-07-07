ca50ae8bea5a99347172bc7a831607b2592cfadb
TarasTurchenko <TurchTas.1551@gmail.com>
2018-07-07 16:47:07 +0300

    FIX: selecting qualifications

--- src/app/components/specialization/specialization-form/specialization-qualification/change-qualification/change-qualification.component.ts
+++ src/app/components/specialization/specialization-form/specialization-qualification/change-qualification/change-qualification.component.ts
@@ -1,14 +1,12 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {SpecializationModalComponent} from '../../../specialization-modal/specialization-modal.component';
 import {Observable} from 'rxjs/Observable';
 import {ProfessionalQualification} from '../../models/professional-qualification';
 import {QualificationService} from '../../services/qualification.service';
 import {getId} from '../../../../../models/basemodels/BaseEntity';
-import {SelectionMode} from '../../enums/selection-mode.enum';
-import {SelectedQualification} from '../../models/selected-qualification';
+import {ModalMargin} from '../../../specialization-modal/models/modal-margin';
 
 import 'rxjs/add/operator/map';
-import {ModalMargin} from '../../../specialization-modal/models/modal-margin';
 
 @Component({
   selector: 'change-qualification',
@@ -18,65 +16,60 @@ import {ModalMargin} from '../../../specialization-modal/models/modal-margin';
 export class ChangeQualificationComponent {
   @Input() canEdit: boolean;
   @Input() qualificationsYear: number;
-  @Output() onSubmit: EventEmitter<SelectedQualification> = new EventEmitter<SelectedQualification>();
+  @Output() onSubmit: EventEmitter<ProfessionalQualification[]> = new EventEmitter<ProfessionalQualification[]>();
+  @Output() allowEditing: EventEmitter<null> = new EventEmitter<null>();
   @ViewChild('modal') modal: SpecializationModalComponent;
-  private _selected: ProfessionalQualification[] = [];
-  private _selectionMode: SelectionMode = SelectionMode.ADD;
-  currentCanEdit: boolean;
+  private selected: ProfessionalQualification[] = [];
   qualifications: Observable<ProfessionalQualification[]>;
+  isOpen: boolean = false;
   modalPadding = new ModalMargin('0', '10px', '0', '10px');
 
   constructor(private _service: QualificationService) {}
 
   open(selected: ProfessionalQualification[]): void {
     this.qualifications = this._service.getAll();
-    this._selected = [...selected];
-    this.currentCanEdit = this.canEdit;
+    this.selected = [...selected];
+    this.isOpen = true;
     this.modal.show();
   }
 
   hide(): void {
     this.modal.hide();
-    this._selected = [];
+    this.selected = [];
+    this.isOpen = false;
   }
 
   submit(): void {
-    const selectedQualification: SelectedQualification = {
-      qualifications: this._selected,
-      selectionMode: this._selectionMode
-    } as SelectedQualification;
-    this.onSubmit.emit(selectedQualification);
-    this._selectionMode = SelectionMode.ADD;
+    this.onSubmit.emit(this.selected);
     this.hide();
   }
 
   select(item: ProfessionalQualification): void {
-    if (this.currentCanEdit) {
+    if (this.canEdit) {
       if (this.isSelected(item.id)) {
-        const itemIndex: number = this._selected.map(getId).indexOf(item.id);
-        this._selected.splice(itemIndex, 1);
+        const itemIndex: number = this.selected.map(getId).indexOf(item.id);
+        this.selected.splice(itemIndex, 1);
       } else {
-        this._selected.push(item);
+        this.selected.push(item);
       }
     }
   }
 
   private isSelected(itemId: number): boolean {
-    return this._selected.map(getId).includes(itemId);
+    return this.selected.map(getId).includes(itemId);
   }
 
   getItemClass(itemId: number): string {
     if (this.isSelected(itemId)) {
       return 'qualification selected can-selected';
     }
-    if (this.currentCanEdit) {
+    if (this.canEdit) {
       return 'qualification can-selected';
     }
     return 'qualification'
   }
 
   createForNewYear(): void {
-    this._selectionMode = SelectionMode.ALL;
-    this.currentCanEdit = true;
+    this.allowEditing.emit();
   }
 }
--- src/app/components/specialization/specialization-form/specialization-qualification/specialization-qualification.component.ts
+++ src/app/components/specialization/specialization-form/specialization-qualification/specialization-qualification.component.ts
@@ -5,10 +5,11 @@ import {ProfessionalQualification} from '../models/professional-qualification';
 import {BaseReactiveFormComponent} from '../../../shared/base-reactive-form/base-reactive-form.component';
 import {QualificationEvents} from '../models/qualification-events';
 import {getId} from '../../../../models/basemodels/BaseEntity';
-import {QualificationForSpecialization, QualificationForSpecializationId} from '../models/QualificationForSpecialization';
-import {SelectedQualification} from '../models/selected-qualification';
-import {SelectionMode} from '../enums/selection-mode.enum';
 import {ChangeQualificationComponent} from './change-qualification/change-qualification.component';
+import {
+  QualificationForSpecialization,
+  QualificationForSpecializationId
+} from '../models/QualificationForSpecialization';
 
 @Component({
   selector: 'specialization-qualification',
@@ -20,7 +21,6 @@ export class SpecializationQualificationComponent extends BaseReactiveFormCompon
   @ViewChild('changeModal') changeModal: ChangeQualificationComponent;
   private events: QualificationEvents;
   private qualificationForSpecializationsIds: QualificationForSpecializationId[] = [];
-  private selectionMode: SelectionMode;
   canEdit = true;
   qualifications: ProfessionalQualification[] = [];
   qualificationsYear: number;
@@ -44,38 +44,17 @@ export class SpecializationQualificationComponent extends BaseReactiveFormCompon
     if (specializationId) {
       this._service.canEdit(specializationId).subscribe((canEdit: boolean) => {
         this.canEdit = canEdit;
-        this.selectionMode = (canEdit) ? SelectionMode.ADD : SelectionMode.ALL;
         this._service.getCurrent(specializationId).subscribe(this.setInitialData.bind(this));
       });
     }
   }
 
   private setInitialData(qualificationForSpecializations: QualificationForSpecialization[]): void {
-    this.qualificationForSpecializationsIds = qualificationForSpecializations.map(this.getQualificationForSpecializationId);
-    this.qualificationsYear = this.getYear(qualificationForSpecializations);
-      this.qualifications = qualificationForSpecializations
+    this.qualificationForSpecializationsIds = qualificationForSpecializations.map(getQualificationForSpecializationId);
+    this.qualificationsYear = getYear(qualificationForSpecializations);
+    this.qualifications = qualificationForSpecializations
       .map((qfs: QualificationForSpecialization) => qfs.professionalQualification);
-    if (this.selectionMode === SelectionMode.ALL) {
-      this.events.addSelected(this.qualifications.map(getId));
-      this.selectionMode = SelectionMode.ADD;
-    }
-  }
-
-  private getYear(qualificationForSpecializations: QualificationForSpecialization[]): number {
-    const firstQualificationForSpecialization = qualificationForSpecializations[0];
-    if (firstQualificationForSpecialization) {
-      return firstQualificationForSpecialization.year;
-    }
-    return 0;
-  }
-
-  private getQualificationForSpecializationId(
-    qualificationForSpecialization: QualificationForSpecialization
-  ): QualificationForSpecializationId {
-    return {
-      id: qualificationForSpecialization.id,
-      qid: qualificationForSpecialization.professionalQualification.id
-    } as QualificationForSpecializationId;
+    this.events.initialIds = this.qualifications.map(getId);
   }
 
   hasData(): boolean {
@@ -84,11 +63,9 @@ export class SpecializationQualificationComponent extends BaseReactiveFormCompon
 
   save(specializationId: number = this.events.specializationId): void {
     this.events.specializationId = specializationId;
-    if (this.events.hasData()) {
-      this._service.save(this.events)
-        .then(() => this.events.clear())
-        .then(() => this.loadData(this.events.specializationId));
-    }
+    this._service.save(this.events)
+      .then(this.events.clear.bind(this.events))
+      .then(() => this.loadData(this.events.specializationId));
   }
 
   create(): void {
@@ -99,54 +76,43 @@ export class SpecializationQualificationComponent extends BaseReactiveFormCompon
     this._service.create(this.form.getRawValue())
       .then((response: ProfessionalQualification) => {
         if (this.updateForm && this.canEdit) {
-          this.events.addSelected(response.id);
+          this.events.selected.push(response.id);
           this.qualifications.push(response);
         }
       }).then(() => this.form.reset());
   }
 
-  changeQualification(selected: SelectedQualification): void {
-    const qualIds = selected.qualifications.map(getId);
-    if (selected.selectionMode === SelectionMode.ALL) {
-      const ids: number[] = selected.qualifications.map(getId);
-      this.events.addSelected(...ids);
-    } else {
-      this.addSelected(qualIds);
-      this.addDeleted(qualIds);
-    }
+  changeQualification(selected: ProfessionalQualification[]): void {
+    const selectedIds = selected.map(getId);
+    this.addSelected(selectedIds);
+    this.addDeleted(selectedIds);
     this.canEdit = true;
-    this.qualifications = selected.qualifications;
+    this.qualifications = selected;
   }
 
-  private addSelected(qualIds: number[]): void {
-    const initialIds = this.qualifications.map(getId);
-    const selectedIds = qualIds.filter(this.isSelected(initialIds));
-    this.events.addSelected(...selectedIds);
+  private addSelected(selectedIds: number[]): void {
+    this.events.selected = selectedIds
+      .filter((selectedId) => !this.events.initialIds.includes(selectedId));
   }
 
-  private isSelected(initialIds: number[]) {
-    return (qualId: number) => {
-      return !initialIds.includes(qualId);
-    }
-  }
-
-  private addDeleted(qualIds: number[]): void {
-    const deletedIds = this.qualificationForSpecializationsIds
-      .filter(this.isDeleted(qualIds))
+  private addDeleted(selectedIds: number[]): void {
+    this.events.deleted = this.qualificationForSpecializationsIds
+      .filter(this.isDeleted(selectedIds).bind(this))
       .map(getId);
-    this.events.addDeleted(...deletedIds);
   }
 
-  private isDeleted(qualIds: number[]) {
+  private isDeleted(selectedIds: number[]) {
     return (qfsId: QualificationForSpecializationId) => {
-      return !qualIds.includes(qfsId.qid);
+      return !selectedIds.includes(qfsId.qid);
     }
   }
 
   createForNewYear(): void {
-    this.changeModal.open(this.qualifications);
-    this.changeModal.createForNewYear();
     this.canEdit = true;
+    this.qualificationForSpecializationsIds = this.events.initialIds = [];
+    if (!this.changeModal.isOpen) {
+      this.changeModal.open(this.qualifications);
+    }
   }
 
   getCreateBtnDesc(): string {
@@ -155,4 +121,22 @@ export class SpecializationQualificationComponent extends BaseReactiveFormCompon
     }
     return (this.canEdit) ? 'Створити та обрати' : 'Створити';
   }
+}
+
+
+function getYear(qualificationForSpecializations: QualificationForSpecialization[]): number {
+  const firstQualificationForSpecialization = qualificationForSpecializations[0];
+  if (firstQualificationForSpecialization) {
+    return firstQualificationForSpecialization.year;
+  }
+  return 0;
+}
+
+function getQualificationForSpecializationId(
+  qualificationForSpecialization: QualificationForSpecialization
+): QualificationForSpecializationId {
+  return {
+    id: qualificationForSpecialization.id,
+    qid: qualificationForSpecialization.professionalQualification.id
+  } as QualificationForSpecializationId;
 }
