5f6b53a5eb3831073096f57b333a9d04b5091e53
gzaspa <gzaspa@ukr.net>
2018-08-30 15:13:35 +0300

    enhance: diploma supplement: process errors

--- src/app/services/diploma-supplement.service.ts
+++ src/app/services/diploma-supplement.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {environment} from '../../environments/environment';
 import {FileService} from './file-service';
+import {catchError} from 'rxjs/operators';
+import {forObservable} from '../components/shared/httpErrors';
 
 @Injectable()
 export class DiplomaSupplementService {
@@ -11,16 +13,16 @@ export class DiplomaSupplementService {
 
   buildDiplomaSupplement(studentId: string): any {
     const url = `${this.documentsUrl}/supplements/degrees/${studentId}/docx`;
-    return this.fileService.downloadFile(url)
+    return this.fileService.downloadFile(url).pipe(catchError(forObservable('Формування додатку до диплома', [])))
   }
 
   buildGradePercent(groupId: string): any {
     const url = `${this.documentsUrl}/percentagereport/groups/${groupId}/docx`;
-    return this.fileService.downloadFile(url)
+    return this.fileService.downloadFile(url).pipe(catchError(forObservable('Формування документу з процентами оцінок студентів', [])))
   }
 
   buildFullGradesTableReport(groupId: string): any {
     const url = `${this.documentsUrl}/grouptablereport/groups/${groupId}`;
-    return this.fileService.downloadFile(url)
+    return this.fileService.downloadFile(url).pipe(catchError(forObservable('Формування шахматки', [])))
   }
 }
--- src/app/services/file-service.ts
+++ src/app/services/file-service.ts
@@ -1,7 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {saveAs} from 'file-saver';
-import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class FileService {
