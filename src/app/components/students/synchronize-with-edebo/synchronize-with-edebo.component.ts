e1bdbabc826a3d782fb96939217451fde25c9268
Alex Ocheretnyi <ochierietnii@gmail.com>
2018-10-09 19:30:36 +0300

    Was created to show modal window with student lists

--- src/app/components/students/synchronize-with-edebo/synchronize-with-edebo.component.ts
+++ src/app/components/students/synchronize-with-edebo/synchronize-with-edebo.component.ts
@@ -2,16 +2,21 @@ import {Component, ViewChild} from '@angular/core';
 import {ModalDirective} from 'ngx-bootstrap';
 import {IAppModal} from '../../shared/modal.interface';
 import {EdeboService} from '../../../services/edebo.service';
+import {StudentDegreeFullEdeboData} from '../../../models/synchronization-edebo-models/StudentDegreeFullEdeboData';
 
 @Component({
   selector: 'synchronize-with-edebo',
   templateUrl: './synchronize-with-edebo.component.html',
-  styleUrls: ['./synchronize-with-edebo.component.scss']
+  styleUrls: ['./synchronize-with-edebo.component.scss'],
 })
 export class SynchronizeWithEdeboComponent implements IAppModal {
   @ViewChild('modal') modal: ModalDirective;
   selectedFile: File = null;
   fileName = 'Вибрати файл';
+  importView = true;
+  modalName = 'Імпортувати файл';
+  modalSize = '';
+  fileProcessingResult: StudentDegreeFullEdeboData;
 
   constructor(private fileUploader: EdeboService) {
   }
@@ -23,7 +28,23 @@ export class SynchronizeWithEdeboComponent implements IAppModal {
 
   onFileUpload(): void {
     let formData = new FormData();
-    formData.append('uploadFile', this.selectedFile, this.selectedFile.name);
-    this.fileUploader.uploadFile(formData).subscribe(res => {});
+    formData.append('file', this.selectedFile, this.selectedFile.name);
+    this.fileUploader.uploadFile(formData).subscribe(res => {this.fileProcessingResult = res});
+    alert(this.fileProcessingResult);
+    this.changeModal();
+  }
+
+  changeToImportModal(): void {
+    this.importView = true;
+    this.modalName = 'Імпортувати файл';
+    this.modalSize = '';
+    this.modal.show();
+  }
+
+  changeModal(): void {
+    this.importView = !this.importView;
+    this.modalName = 'Студенти';
+    this.modalSize = 'modal-full';
+
   }
 }
--- src/app/services/edebo.service.ts
+++ src/app/services/edebo.service.ts
@@ -1,5 +1,5 @@
-import {Injectable, Input} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Observable} from 'rxjs/Observable';
 
@@ -8,12 +8,12 @@ export class EdeboService {
   private url = `${environment.apiUrl}/students/edebo-synchronization`;
   constructor(private _http: HttpClient) { }
 
-  public uploadFile(formdata: any) {
-    return this._http.post(this.url, formdata)
+  uploadFile(formData: any): Observable<any> {
+    return this._http.post(this.url, formData)
       .catch(this._errorHandler);
   }
 
-  public _errorHandler(error: Response) {
+   _errorHandler(error: Response) {
     console.error('Error Occured: ' + error);
     return Observable.throw(error || 'Some Error on Server Occured');
   }
