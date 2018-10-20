ddc6c6a30aa29e6c7138a37e665ad7345e3cdfa8
Roman Krivenko <roman-kr-2017@ukr.net>
2018-10-20 23:36:19 +0300

    refactoring: style of columns to translations

--- src/app/components/group/translations.js
+++ src/app/components/group/translations.js
@@ -1,12 +1,52 @@
 export const translations = [
-  {field: [['name']], title: 'Назва', className: 'name'},
-  {field: [['specialization', 'name']], title: 'Спеціалізація', className: 'specialization'},
-  {field: [['specialization', 'speciality', 'code'], ['specialization', 'speciality', 'name']], title: 'Спеціальність', className: 'speciality'},
-  {field: [['specialization', 'degree', 'name']], title: 'Ступінь', className: 'degree'},
-  {field: [['tuitionForm']], title: 'Форма', className: 'tuitionForm'},
-  {field: [['tuitionTerm']], title: 'Термін', className: 'tuitionTerm'},
-  {field: [['creationYear']], title: 'Створено', className: 'creationYear'},
-  {field: [['beginYears']], title: 'Курс початку', className: 'beginYears'},
-  {field: [['studyYears']], title: 'Років навчання', className: 'studyYears'},
-  {field: [['studySemesters']], title: 'Семестрів', className: 'studySemesters'},
+  {
+    field: [['name']],
+    title: 'Назва',
+    style: {flex: '0 0 100px'}
+  },
+  {
+    field: [['specialization', 'name']],
+    title: 'Спеціалізація',
+    style: {flex: '1 0 200px'}
+  },
+  {
+    field: [['specialization', 'speciality', 'code'], ['specialization', 'speciality', 'name']],
+    title: 'Спеціальність',
+    style: {flex: '1 0 220px'}
+  },
+  {
+    field: [['specialization', 'degree', 'name']],
+    title: 'Ступінь',
+    style: {flex: '0 0 100px'}
+  },
+  {
+    field: [['tuitionForm']],
+    title: 'Форма',
+    style: {flex: '0 0 115px'}
+  },
+  {
+    field: [['tuitionTerm']],
+    title: 'Термін',
+    style: {flex: '0 0 100px'}
+  },
+  {
+    field: [['creationYear']],
+    title: 'Створено',
+    style: {flex: '0 0 105px'}
+  },
+  {
+    field: [['beginYears']],
+    title: 'Курс початку',
+    style: {flex: '0 0 95px'}
+  },
+  {
+    field: [['studyYears']],
+    title: 'Років навчання',
+    style: {flex: '0 0 106px'}
+  },
+  {
+    field: [['studySemesters']],
+    title: 'Семестрів',
+    style: {flex: '0 0 110px'}
+  },
 ];
