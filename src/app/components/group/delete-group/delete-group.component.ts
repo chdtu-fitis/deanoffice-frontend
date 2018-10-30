da10e7c635d5b1715cd9abf616183a4cab8c676e
Roman Krivenko <roman-kr-2017@ukr.net>
2018-10-30 21:34:13 +0200

    enhance: style in delete-group

--- src/app/components/group/delete-group/delete-group.component.ts
+++ src/app/components/group/delete-group/delete-group.component.ts
@@ -2,8 +2,7 @@ import {Component, EventEmitter, Output, ViewChild} from '@angular/core';
 import {GroupService} from '../../../services/group.service';
 import {GroupModalComponent} from '../group-modal/group-modal.component'
 import {StudentGroup} from '../../../models/StudentGroup';
-
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'delete-group',
@@ -11,9 +10,11 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./delete-group.component.scss']
 })
 export class DeleteGroupComponent {
-  groups: StudentGroup[];
+
   @Output() onSubmit: EventEmitter<any> = new EventEmitter<any>();
   @ViewChild('modal') modal: GroupModalComponent;
+
+  groups: StudentGroup[];
 
   form = new FormGroup({});
 
