919932119bb405dad9e4ad4546b1224bfdba5b79
AlexOcheretnyi <ochierietnii@gmail.com>
2018-11-21 09:53:05 +0200

    develop: stud thesis theme: view elements with simple logic

--- src/app/components/students/student-thesis-theme-input/student-thesis-theme-input.component.ts
+++ src/app/components/students/student-thesis-theme-input/student-thesis-theme-input.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {ModalDirective} from 'ngx-bootstrap';
+import {ThesisInputService} from '../../../services/thesis-input.service';
 
 @Component({
   selector: 'student-thesis-theme-input',
@@ -11,8 +12,12 @@ export class StudentThesisThemeInputComponent implements OnInit {
   fileName = 'Виберіть файл';
   selectedFile: File = null;
   wrongExtension = false;
+  uploadInProgress = false;
+  fileField = true;
+  modalSize = '';
+  tableView = false;
 
-  constructor() { }
+  constructor(private thesisService: ThesisInputService) { }
 
   ngOnInit() {
   }
@@ -28,5 +33,36 @@ export class StudentThesisThemeInputComponent implements OnInit {
     this.wrongExtension = this.checkExtension(this.selectedFile);
     this.fileName = this.selectedFile.name;
   }
+
+  onFileUpload () {
+    this.uploadInProgress = true;
+    this.fileField = false;
+    const formData = new FormData();
+    formData.append('file', this.selectedFile, this.selectedFile.name);
+
+    this.thesisService.uploadFile(formData).subscribe(
+      res => {
+
+        this.uploadInProgress = false;
+        this.changeModal();
+      }
+    );
+  }
+
+  changeModal () {
+    this.modalSize = 'modal-full';
+    this.tableView = true;
+  }
+
+  // setNumberOfStudents(tableType): void {
+  //   switch (tableType) {
+  //     case 'green':
+  //       this.studentsInTable = this.synchronizedStudentDegreesGreen.length;
+  //       break;
+  //     case 'blue':
+  //       this.studentsInTable = this.unmatchedSecondaryDataStudentDegreesBlue.length;
+  //       break;
+  //   }
+  // };
 
 }
