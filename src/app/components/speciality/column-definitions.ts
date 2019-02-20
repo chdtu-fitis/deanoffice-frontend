c5b79794902cb8b1a5fcee78402da020b172b695
Andriy Orehov <andriyorehov@gmail.com>
2019-02-20 17:11:37 +0200

    refactoring: use ag-grid at speciality

---
+++ src/app/components/speciality/column-definitions.ts
@@ -0,0 +1,30 @@
+export const COLUMN_DEFINITIONS = [
+  {
+    headerName: 'Шифр',
+    field: 'code',
+    checkboxSelection: true,
+  },
+  {
+    headerName: 'Назва',
+    field: 'name',
+  },
+  {
+    headerName: 'Назва Англійською',
+    field: 'nameEng',
+  },
+  {
+    headerName: 'Галузь Знань',
+    field: 'fieldOfStudy',
+  },
+  {
+    headerName: 'Галузь Знань Англійською',
+    field: 'fieldOfStudyEng',
+  }
+];
+
+
+
+
+
+
+
