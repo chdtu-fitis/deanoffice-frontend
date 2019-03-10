e2a286a7347e38dc8fcb3ab16852857010f22109
Andriy Orehov <andriyorehov@gmail.com>
2019-03-10 17:33:32 +0200

    refactor: students-table: add typing

--- src/app/components/students/students-table/students-table.component.ts
+++ src/app/components/students/students-table/students-table.component.ts
@@ -1,10 +1,12 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {GridReadyEvent, ModelUpdatedEvent, SelectionChangedEvent} from 'ag-grid-community'
+
 import {StudentDegree} from '../../../models/StudentDegree';
 import {defaultColDef, LOCALE_TEXT} from '../constants';
 import {PaymentFilterComponent} from '../payment-filter/payment-filter.component';
 
 @Component({
-  selector: 'expelled-students-table',
+  selector: 'students-table',
   templateUrl: './students-table.component.html',
   styleUrls: ['./students-table.component.scss']
 })
@@ -26,27 +28,27 @@ export class StudentsTableComponent {
     };
   }
 
-  onGridReady(params) {
+  onGridReady(params: GridReadyEvent) {
     this.gridApi = params.api;
     this.gridColumnApi = params.columnApi;
     this.gridApi.sizeColumnsToFit();
   }
 
-  onSelectionChanged() {
-    const selected = this.gridApi.getSelectedRows();
+  onSelectionChanged(event: SelectionChangedEvent) {
+    const selected = event.api.getSelectedRows();
     this.selectionChanged.emit(selected)
   }
 
-  onModelUpdated(params) {
+  onModelUpdated(params: ModelUpdatedEvent) {
     const count = params.api.getDisplayedRowCount();
     this.modelUpdated.emit(count);
   }
 
-  onRenew(forRenew) {
+  onRenew(forRenew: StudentDegree[]) {
     this.gridApi.updateRowData({ remove: forRenew });
   }
 
-  showByIndex(index) {
+  showByIndex(index: number) {
     if (index !== -1) {
       this.gridApi.ensureIndexVisible(index, 'top');
       const node = this.gridApi.getRowNode(this.rows[index].id);
