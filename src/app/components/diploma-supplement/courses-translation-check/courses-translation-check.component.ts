07f72dd7d079085b27470d5ddeeefa4255cf9138
Vlad Muzychenko <pexterexpert@gmail.com>
2019-03-26 16:33:24 +0200

    add: show courses with no translation for graduates: data class and fill component ts

--- src/app/components/diploma-supplement/courses-translation-check/courses-translation-check.component.ts
+++ src/app/components/diploma-supplement/courses-translation-check/courses-translation-check.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {DataForSupplementStudentCheck} from '../../../models/custom/DataForSupplementStudentCheck';
+import {BsModalRef} from 'ngx-bootstrap';
 
 @Component({
   selector: 'courses-translation-check',
   templateUrl: './courses-translation-check.component.html',
   styleUrls: ['./courses-translation-check.component.scss']
 })
-export class CoursesTranslationCheckComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class CoursesTranslationCheckComponent {
+  coursesTranslationCheckData: DataForSupplementStudentCheck[];
+  header: String;
+  constructor(public bsModalRef: BsModalRef) {}
 }
