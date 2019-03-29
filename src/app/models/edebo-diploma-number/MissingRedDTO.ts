d6f36bff7b06c148834869e50809a48dc81ef567
AlexOcheretnyi <ochierietnii@gmail.com>
2019-03-29 10:19:14 +0200

    164-edebo-diploma-number: add:  MissingRedDTO model

---
+++ src/app/models/edebo-diploma-number/MissingRedDTO.ts
@@ -0,0 +1,6 @@
+import {DiplomaAndSynchronizedStudentDTO} from './DiplomaAndSynchronizedStudentDTO';
+
+export class MissingRedDTO {
+  message: string;
+  diplomaSynchronizedData: DiplomaAndSynchronizedStudentDTO;
+}
