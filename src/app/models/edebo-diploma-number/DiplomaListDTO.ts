69820b2d6259300a2a33ab2832674037c35322fd
AlexOcheretnyi <ochierietnii@gmail.com>
2019-03-29 10:22:32 +0200

    164-edebo-diploma-number: add:  transfer object model from Diploma Number Controller

---
+++ src/app/models/edebo-diploma-number/DiplomaListDTO.ts
@@ -0,0 +1,7 @@
+import {DiplomaAndSynchronizedStudentDTO} from './DiplomaAndSynchronizedStudentDTO';
+import {MissingRedDTO} from './MissingRedDTO';
+
+export class DiplomaListDTO {
+  diplomaSynchrozedDTOs: DiplomaAndSynchronizedStudentDTO;
+  missingRed: MissingRedDTO;
+}
