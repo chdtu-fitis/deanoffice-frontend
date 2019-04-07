eb6ac3001d11082b96c1e301b12db78a51d86b66
AlexOcheretnyi <ochierietnii@gmail.com>
2019-04-07 22:43:32 +0300

    #209-feat/departments: add:  delete-department.component

---
+++ src/app/components/department/delete-department/delete-department.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'delete-department',
+  templateUrl: './delete-department.component.html',
+  styleUrls: ['./delete-department.component.scss']
+})
+export class DeleteDepartmentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
