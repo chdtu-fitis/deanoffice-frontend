ddad0d2fbaf4c6e4110b5b2eb2ee19728111e2e8
AlexOcheretnyi <ochierietnii@gmail.com>
2019-04-07 21:35:38 +0300

    #209-feat/departments: add: department-form.component

---
+++ src/app/components/department/department-form/department-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'department-form',
+  templateUrl: './department-form.component.html',
+  styleUrls: ['./department-form.component.scss']
+})
+export class DepartmentFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
