6706ef5ad670f91d60300dfe72e53ac4e712cfde
Artem Logvynenko <artuom.logv@gmail.com>
2019-05-13 16:57:57 +0300

    #255 teachers: enhance: change createteacher to create in pipes

--- src/app/pipes/maps-keys.pipe.spec.ts
+++ src/app/pipes/maps-keys.pipe.spec.ts
@@ -1,7 +1,7 @@
 import { MapsKeysPipe } from './maps-keys.pipe';
 
 describe('MapsKeysPipe', () => {
-  it('createTeacher an instance', () => {
+  it('create an instance', () => {
     const pipe = new MapsKeysPipe();
     expect(pipe).toBeTruthy();
   });
