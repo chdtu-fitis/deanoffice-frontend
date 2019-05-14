12da0ed01d9c7dfb5e1e9cbab3e325226acc998a
Andriy Orehov <andriyorehov@gmail.com>
2019-05-15 01:21:10 +0300

    #272 grade-statement-table: refactor: use view property on Grade model instead of DOM manipulation

--- src/app/components/grade/grade-statement/grade-statement-table/grade-statement-table.component.ts
+++ src/app/components/grade/grade-statement/grade-statement-table/grade-statement-table.component.ts
@@ -55,33 +55,28 @@ export class GradeStatementTableComponent {
     }
 
     editGrade(studentDegreeId: number, studentId: number, grade: Grade, e: any): void {
-        const elementId = this.getElementId(studentDegreeId);
-        const studentDegree = this.studentsDegree[studentId];
-        const points = Number(e.target.valueAsNumber || e.target.value);
-        if (points > 100 || points < 0 || !points) {
-            this.updateVisible(elementId, 'bg-danger');
-            this.setError('Помилка, оцiнка повинна бути бiльша 0 та менша або рiвна 100!');
-        } else {
-            this.setError('');
-            if (studentDegree.grade.points !== points) {
-                if (!studentDegree.grade.points) {
-                    studentDegree.grade.onTime = true;
-                }
-                studentDegree.grade.points = points;
-                delete studentDegree.grade.empty;
-                this.setUpdateGrades(studentDegree);
-                this.updateVisible(elementId, 'bg-warning');
-            }
+      const studentDegree = this.studentsDegree[studentId];
+      const points = Number(e.target.valueAsNumber || e.target.value);
+      if (points > 100 || points < 0 || !points) {
+        grade.wrongInterval = true;
+        grade.changed = true;
+        this.setError('Помилка, оцiнка повинна бути бiльша 0 та менша або рiвна 100!');
+      } else {
+        this.setError('');
+        if (studentDegree.grade.points !== points) {
+          if (!studentDegree.grade.points) {
+              studentDegree.grade.onTime = true;
+          }
+          studentDegree.grade.points = points;
+          delete studentDegree.grade.empty;
+          this.setUpdateGrades(studentDegree);
+          grade.wrongInterval = false;
+          grade.changed = true;
         }
+      }
     }
 
     setError(error: string): void {
         this.error.emit(error);
-    }
-
-    updateVisible(id, style): void {
-        const element = document.getElementById(id).parentElement;
-        const styles = 'text-center align-middle';
-        element.setAttribute('class', `${styles} ${style}`);
     }
 }
