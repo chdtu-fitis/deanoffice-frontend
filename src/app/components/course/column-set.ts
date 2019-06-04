e644c79eae6c721a2f9e5cc7e2fdb13f83414781
Vlad Muzychenko <pexterexpert@gmail.com>
2019-06-04 10:45:52 +0300

    230 display-exist-courses: fix: the knowledge control display fix

--- src/app/components/course/column-set.ts
+++ src/app/components/course/column-set.ts
@@ -2,25 +2,30 @@ export const COLUMN_DEFINITIONS = [
   {
     headerName: 'Назва',
     field: 'courseName.name',
+    width: 400,
   },
   {
     headerName: 'Семестер',
     field: 'semester',
+    width: 120,
   },
   {
     headerName: 'Тип контролю',
     field: 'knowledgeControl.name',
+    width: 170,
   },
   {
     headerName: 'Години',
     field: 'hours',
+    width: 80,
   },
   {
     headerName: 'Кредити',
     field: 'credits',
+    width: 80,
   },
   {
     headerName: 'Годин на кредит',
-    field: 'hoursPerCreditList',
+    field: 'hoursPerCredit',
   }
 ];
