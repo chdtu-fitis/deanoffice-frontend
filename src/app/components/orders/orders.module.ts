47be7c0143d877a023f26231fad22d8aaccf412d
Oleksandr Ocheretnyi <ochierietnii@gmail.com>
2019-10-07 21:48:04 +0300

    added orders module

---
+++ src/app/components/orders/orders.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { OrdersRoutingModule } from './orders-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    OrdersRoutingModule
+  ],
+  declarations: []
+})
+export class OrdersModule { }
