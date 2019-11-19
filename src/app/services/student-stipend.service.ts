012ce13717430cf1976fa83778d326e64d78ad84
YulyKo <juliakotenko.forstudy@gmail.com>
2019-11-19 17:17:30 +0200

    #353 new-feature: set button for download rating was done

--- src/app/services/student-stipend.service.ts
+++ src/app/services/student-stipend.service.ts
@@ -2,13 +2,14 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {StudentStipendInfo} from "../models/student-stipend/StudentStipendInfo";
+import {StudentStipendInfo} from '../models/student-stipend/StudentStipendInfo';
+import {FileService} from './file-service';
 
 @Injectable()
 export class StudentStipendService {
   private studentStipendUrl = `${environment.apiUrl}/student-degree/stipend`;
-
-  constructor(private http: HttpClient) {
+  private ratingPapersUrl = `${environment.apiUrl}`;
+  constructor(private http: HttpClient, private fileService: FileService) {
   }
 
   getStudentsStipendInfo(): Observable<StudentStipendInfo[]> {
@@ -16,5 +17,10 @@ export class StudentStipendService {
   }
   sendExtraPoints(array) {
     return this.http.post(`${this.studentStipendUrl}/extra-points-update`, array);
+  }
+
+  buildRatingPapers(): any {
+    const url = this.ratingPapersUrl;
+    return this.fileService.downloadFile(url);
   }
 }
