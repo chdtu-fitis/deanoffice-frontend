3e7082cf13f306e759f65c3f5d60045ae1a209b7
Oleksandr Ocheretnyi <ochierietnii@gmail.com>
2020-03-28 10:14:35 +0200

    template updates

--- src/app/components/orders/add-order/add-order.component.ts
+++ src/app/components/orders/add-order/add-order.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+
 import {ModalDirective} from 'ngx-bootstrap';
-import {orderTypes} from '../moc';
+
+import {OrdersService} from '../../../services/orders.service';
 
 @Component({
   selector: 'add-order',
@@ -8,21 +11,36 @@ import {orderTypes} from '../moc';
   styleUrls: ['./add-order.component.scss']
 })
 export class AddOrderComponent implements OnInit {
-
   @ViewChild('modal', {static: false}) modal: ModalDirective;
 
-  public orderTypes = orderTypes;
-  constructor() { }
+  public creatOrderForm: FormGroup;
+  public orderTypes;
+  constructor(private fb: FormBuilder,
+              private _ordersService: OrdersService) { }
 
+  async ngOnInit() {
+    this._initForm();
 
-  ngOnInit() {
+    this.orderTypes = await this._ordersService.getOrderTypes().toPromise();
+
   }
 
-  hideModal() {
+  public hideModal(): void {
     this.modal.hide();
   }
 
-  changeOrderType(orderType) {
+  public onSubmit(): void {
+
+  }
+
+  public changeOrderType(orderType): void {
+    console.log(orderType);
+  }
+
+  private _initForm(): void {
+    this.creatOrderForm = this.fb.group({
+      orderType: ['Про зміну ім\'я']
+    })
   }
 
 }
--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
-import {environment} from '../../environments/environment';
 import {HttpClient} from '@angular/common/http';
-import {OrdersControls} from '../components/orders/orders-types';
+
 import {Observable} from 'rxjs/Observable';
-import {catchError} from 'rxjs/operators';
-import {forObservable} from '../components/shared/httpErrors';
 import {of} from 'rxjs/observable/of';
-import {tableData} from '../components/orders/moc';
+
+import {environment} from '../../environments/environment';
+import {OrdersControls} from '../components/orders/orders-types';
+import {orderTypes, tableData} from '../components/orders/moc';
 
 @Injectable()
 export class OrdersService {
@@ -22,5 +22,10 @@ export class OrdersService {
     return of(tableData);
     // return this.http.get(url)
     //   .pipe(catchError(forObservable('Отримання наказів по факультету', [])));
+  }
+
+  public getOrderTypes(): Observable<any> {
+    // return this.http.get(this.url)
+    return of(orderTypes);
   }
 }
