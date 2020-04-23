7370ed60ba2d5337bbc3e7053f17966cf8b364e3
gzaspa <mshvenra5>
2020-04-23 12:00:40 +0300

    student degree data synchronization with EDEBO: develop: import tuition term and tuition form for new students

--- src/app/models/synchronization-edebo-models/StudentDegreeFullEdeboData.ts
+++ src/app/models/synchronization-edebo-models/StudentDegreeFullEdeboData.ts
@@ -3,6 +3,8 @@ import {Student} from './Student';
 import {Specialization} from './Specialization';
 import {DiplomaType} from '../diploma-type.enum';
 import {Payment} from '../payment.enum';
+import {TuitionForm} from "../tuition-form.enum";
+import {TuitionTerm} from "../tuition-term.enum";
 
 export class StudentDegreeFullEdeboData {
   id?: number;
@@ -17,6 +19,8 @@ export class StudentDegreeFullEdeboData {
   admissionOrderNumber?: string;
   admissionOrderDate?: Date;
   payment?: Payment;
+  tuitionForm: TuitionForm;
+  tuitionTerm: TuitionTerm;
   modified?: boolean;
   selected?: boolean;
   similarInDB?: boolean;
