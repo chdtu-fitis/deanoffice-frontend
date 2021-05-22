b9f55146a7d86ff35a1dba2a45f79019b59c3477
deand0n <57573532+deand0n@users.noreply.github.com>
2021-05-22 15:56:02 +0300

    changed ngModel to FormControl, added: error messages, surname validation

--- src/app/components/selective-course/edit-student-dialog/edit-student-dialog.component.ts
+++ src/app/components/selective-course/edit-student-dialog/edit-student-dialog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {BsModalRef} from 'ngx-bootstrap/modal';
 import {SelectiveCourseService} from '../../../services/selective-course.service';
 import {SelectiveCoursesStudentDegree} from '../../../models/SelectiveCoursesStudentDegree';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'edit-student-dialog',
@@ -12,8 +13,12 @@ export class EditStudentDialogComponent implements OnInit {
 
   studyYear: string;
   selectiveCoursesStudentDegree: SelectiveCoursesStudentDegree[] = [];
-  surname = '';
   isButtonClicked = false;
+
+  form = new FormGroup({
+    'surname': new FormControl('', [Validators.required]),
+  })
+
 
   constructor(public bsModalRef: BsModalRef,
               private selectiveCourseService: SelectiveCourseService) { }
@@ -23,10 +28,14 @@ export class EditStudentDialogComponent implements OnInit {
 
   findStudentCourses(all: boolean): void {
     this.selectiveCoursesStudentDegree = [];
-    this.selectiveCourseService.getStudentCoursesBySurname(true, this.studyYear, this.surname)
-      .subscribe(selectiveCoursesStudentDegree => {
-        this.selectiveCoursesStudentDegree = selectiveCoursesStudentDegree;
-      })
+    if (this.form.valid) {
+      this.selectiveCourseService.getStudentCoursesBySurname(true, this.studyYear, this.f.surname.value)
+        .subscribe(selectiveCoursesStudentDegree => {
+          this.selectiveCoursesStudentDegree = selectiveCoursesStudentDegree;
+        })
+    }
     this.isButtonClicked = true;
   }
+
+  get f() { return this.form.controls; }
 }
