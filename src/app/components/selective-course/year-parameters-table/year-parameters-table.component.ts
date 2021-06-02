871852c9d8899f5b3f5cb2c0f4705da87b9d7825
Max <jeserodri7@gmail.com>
2021-06-02 20:00:59 +0300

    #489 mark current selective courses period: add: add class, variables and html

--- src/app/components/selective-course/year-parameters-table/year-parameters-table.component.ts
+++ src/app/components/selective-course/year-parameters-table/year-parameters-table.component.ts
@@ -1,8 +1,6 @@
 import { ConstantPool } from '@angular/compiler';
 import {Component, Input, OnInit} from '@angular/core';
 import {SelectiveCoursesYearParameters} from '../../../models/SelectiveCoursesYearParameters';
-import {SelectiveCourseService} from '../../../services/selective-course.service';
-import {selectiveCourseRoutes} from '../selective-course.module';
 
 @Component({
   selector: 'year-parameters-table',
@@ -12,11 +10,29 @@ import {selectiveCourseRoutes} from '../selective-course.module';
 export class YearParametersTableComponent implements OnInit {
 
   @Input() yearParameters: SelectiveCoursesYearParameters[];
+  today: Date;
+  isFirstPeriodFirstRound: boolean;
+  isFirstPeriodBetweenFirstAndSecondRound: boolean;
+  isFirstPeriodSecondRound: boolean;
+  isSecondPeriodFirstRound: boolean;
+  isSecondPeriodBetweenFirstAndSecondRound: boolean;
+  isSecondPeriodSecondRound: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    this.today = new Date();
+    this.isFirstPeriodFirstRound = (this.today >= new Date(this.yearParameters[0].firstRoundStartDate))
+      && (this.today <= new Date(this.yearParameters[0].firstRoundEndDate));
+    this.isFirstPeriodBetweenFirstAndSecondRound = (this.today > new Date(this.yearParameters[0].firstRoundEndDate))
+      && (this.today < new Date(this.yearParameters[0].secondRoundStartDate));
+    this.isFirstPeriodSecondRound = (this.today >= new Date(this.yearParameters[0].secondRoundStartDate))
+      && (this.today <= new Date(this.yearParameters[0].secondRoundEndDate));
+    this.isSecondPeriodFirstRound = (this.today >= new Date(this.yearParameters[1].firstRoundStartDate))
+      && (this.today <= new Date(this.yearParameters[1].firstRoundEndDate));
+    this.isSecondPeriodBetweenFirstAndSecondRound = (this.today > new Date(this.yearParameters[1].firstRoundEndDate))
+      && (this.today < new Date(this.yearParameters[1].secondRoundStartDate));
+    this.isSecondPeriodSecondRound = (this.today >= new Date(this.yearParameters[1].secondRoundStartDate))
+      && (this.today <= new Date(this.yearParameters[1].secondRoundEndDate));
   }
-
-
 }
