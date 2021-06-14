4949bfaaef80988e59042e96fbe7cadbaeebfbb3
deand0n <57573532+deand0n@users.noreply.github.com>
2021-06-14 16:55:23 +0300

    #480 edit students selective courses: add: new models: ExistingId, SelectiveCoursesStudentDegreeSubstitution, SelectiveCoursesStudentDegreeWithStudyYear

---
+++ src/app/models/ExistingId.ts
@@ -0,0 +1,3 @@
+export class ExistingId {
+  id: number;
+}
---
+++ src/app/models/SelectiveCoursesStudentDegreeSubstitution.ts
@@ -0,0 +1,8 @@
+import {ExistingId} from './ExistingId';
+
+export class SelectiveCoursesStudentDegreeSubstitution {
+  selectiveCoursesIdsToAdd: number[];
+  selectiveCoursesIdsToDrop: number[];
+  studentDegree: ExistingId;
+  studyYear: number;
+}
