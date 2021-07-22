3bfe4543c0863821b87223ebf9f54f01e20b31a3
gzaspa <mshvenra5>
2021-07-22 17:23:48 +0300

    480 edit student selective courses: develop: add isBeingEdited attribute to selective courses

--- src/app/components/selective-course/edit-student-dialog/student-courses-table/student-courses-table.component.ts
+++ src/app/components/selective-course/edit-student-dialog/student-courses-table/student-courses-table.component.ts
@@ -1,6 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {SelectiveCoursesStudentDegree} from '../../../../models/SelectiveCoursesStudentDegree';
 import {TypeCycle} from '../../../../models/TypeCycle';
+import {SelectiveCourse} from "../../../../models/SelectiveCourse";
+import {SelectiveCourseService} from "../../../../services/selective-course.service";
 
 @Component({
   selector: 'student-coures-table',
@@ -11,28 +13,46 @@ export class StudentCoursesTableComponent implements OnInit {
 
   @Input() selectiveCoursesStudentDegrees: SelectiveCoursesStudentDegree[];
   typeCycle = TypeCycle;
-  isBeingEdited: boolean[] = [];
   searchText: string;
+  selectiveCourses: SelectiveCourse[];
 
-  constructor() {
+  constructor(private selectiveCourseService: SelectiveCourseService) {
   }
 
   ngOnInit() {
     for (let selectiveCoursesStudentDegree of this.selectiveCoursesStudentDegrees) {
-      this.isBeingEdited.push(false);
+      for (let selectiveCourse of selectiveCoursesStudentDegree.selectiveCourses) {
+        (selectiveCourse as any).isBeingEdited = false;
+      }
     }
   }
 
-  substituteCourse(i: number): void {
-    this.isBeingEdited[i] = true;
-    console.log(this.isBeingEdited);
+  editSubstituteCourse(i: number, j: number): void {
+    this.getSelectiveCourse(i, j).isBeingEdited = true;
   }
 
   saveSubstituteCourse(i: number): void {
     console.log('save');
   }
 
-  cancelSubstituteCourse(i: number): void {
-    this.isBeingEdited[i] = false;
+  cancelSubstituteCourse(i: number, j: number): void {
+    this.getSelectiveCourse(i, j).isBeingEdited = false;
+  }
+
+  getSelectiveCourses(i: number, j: number) {
+    if (!this.selectiveCourses && !this.selectiveCourses.length) {
+
+    }
+    let degreeId = this.selectiveCoursesStudentDegrees[i].studentDegree.specialization.degree.id;
+    // this.selectiveCoursesStudentDegrees[i].selectiveCourses
+    // this.selectiveCourseService.getSelectiveCourses(this.studyYear, degreeId, this.semester, true)
+    //   .subscribe((selectiveCourses: SelectiveCourse[]) => {
+    //
+    //     this.selectiveCourses = selectiveCourses;
+    //   });
+  }
+
+  getSelectiveCourse(i: number, j: number): any {
+    return this.selectiveCoursesStudentDegrees[i].selectiveCourses[j] as any;
   }
 }
