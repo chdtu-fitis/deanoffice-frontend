ba0e2c23e9090da17516becddb7be5f74dc00277
Olexandr Stepanenko <sashok1111122222@gmail.com>
2022-04-07 18:21:04 +0300

    526 students statistics of selective courses: add: left and right sides of component, bare and bones of table and buttons, ability to export table into xlsx format

--- src/app/components/selective-course/students-statistics-of-selective-courses/students-statistics-of-selective-courses.component.ts
+++ src/app/components/selective-course/students-statistics-of-selective-courses/students-statistics-of-selective-courses.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {BsModalRef} from 'ngx-bootstrap/modal';
 import {StudentDegree} from '../../../models/StudentDegree';
+import {Degree} from '../../../models/Degree';
+import {DegreeService} from '../../../services/degree.service';
+import * as XLSX from "xlsx";
 
 @Component({
   selector: 'students-statistics-of-selective-courses',
@@ -9,9 +12,50 @@ import {StudentDegree} from '../../../models/StudentDegree';
 })
 export class StudentsStatisticsOfSelectiveCoursesComponent implements OnInit {
   students: StudentDegree[];
-  constructor(public bsModalRef: BsModalRef) { }
+  testNumber = ["fetr", "fitis", "feu", "fktmd"];
+  degrees: Degree[] = [];
+  currentDegree: Degree;
+  averagePercent = 100
+  fileName = "ExcelSheet.xlsx";
+
+  constructor(public bsModalRef: BsModalRef, private degreeService: DegreeService,) { }
 
   ngOnInit() {
+
+    this.degreeService.getDegrees().subscribe(degrees => {
+      this.degrees = degrees;
+      if (this.degrees) {
+        this.currentDegree = this.degrees[0];
+        this.onDegreeChange();
+      }
+    });
+
+
+  }
+
+  onDegreeChange() {
+    console.log(this.currentDegree)
+  }
+
+  showPercentOfStudentsWhoDidChoice() {
+
+  }
+
+  showListOfStudentsWhoDidNotChoice() {
+
+  }
+  studentsWithUnexpectedAmountOfCourses() {
+
+  }
+
+  exportTableIntoCsv(): void {
+    let element = document.getElementById("excel-table");
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Statistics");
+
+    XLSX.writeFile(wb, this.fileName)
   }
 
 }
