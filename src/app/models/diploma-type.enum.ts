86cb03339a47549d85f712e22abe94f153c675bd
gzaspa <mshvenra5>
2022-10-19 14:23:22 +0300

    student edebo synchronization: add: make DiplomaType match the backend

--- src/app/models/diploma-type.enum.ts
+++ src/app/models/diploma-type.enum.ts
@@ -1,6 +1,6 @@
 export enum DiplomaType {
   SECONDARY_SCHOOL_CERTIFICATE = 'Атестат про повну загальну середню освіту',
-  JUNIOR_BACHELOR_DIPLOMA = 'Диплом молодшого спеціаліста',
+  JUNIOR_SPECIALIST_DIPLOMA = 'Диплом молодшого спеціаліста',
   BACHELOR_DIPLOMA = 'Диплом бакалавра',
   MASTER_DIPLOMA = 'Диплом магістра',
   PHD_DIPLOMA = 'Диплом доктора філософії',
@@ -9,5 +9,7 @@ export enum DiplomaType {
   QUALIFIED_WORKER_DIPLOMA = 'Диплом кваліфікованого робітника',
   WORKER_QUALIFICATION_DIPLOMA = 'Свідоцтво про присвоєння(підвищення) робітничої кваліфікації',
   SPECIALIST_DIPLOMA = 'Диплом спеціаліста',
-  SECONDARY_SCHOOL_CERTIFICATE1 = 'Свідоцтво про здобуття повної загальної середньої освіти'
+  SECONDARY_SCHOOL_CERTIFICATE1 = 'Свідоцтво про здобуття повної загальної середньої освіти',
+  TEMPORARY_DONBAS_CERTIFICATE = 'Довідка (тимчасова) про повну загальну середню освіту (ОЦ «Донбас-Україна»)',
+  JUNIOR_BACHELOR_DIPLOMA = 'Диплом молодшого спеціаліста',
 }
